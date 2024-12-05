d66df40c9a4843bf599832aefd76185129b68143
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2024-12-05 12:49:46 +0530

    create home.jsx file

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BedtimeIcon from '@mui/icons-material/Bedtime';
+import { Header } from '../components/Header';
+
+export const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
