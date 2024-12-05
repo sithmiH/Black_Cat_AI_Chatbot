78b528ffc03dc84bcaa4e8ba37f14340832a1a0d
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2024-12-05 17:49:03 +0530

    create Side panel and Prompt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Prompt from "./components/Prompt";
+import SidePanel from "./components/SidePanel";
 
 function App() {
   return (
-    <div className="App">
-      <p>hiii</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Prompt />
+        {/* You can add more routes here if you want to switch between components */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+            backgroundColor: "#f5f5f5",
+          }}
+        >
+          {/* Add your MathMasterCard component here if it's not yet imported */}
+          {/* Example: */}
+          <SidePanel />
+        </div>
+      </div>
+    </Router>
   );
 }
 
