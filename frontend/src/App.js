39ead6c359989b63f5e04b95815b84fd31527681
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2024-12-05 18:48:39 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
-import "./App.css";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <p>hiii</p> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <p>hiii</p>
+    <div
+      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    >
+      <Header />
+      <Container component="main" sx={{ flexGrow: 1, py: 4 }}>
+        {/* Main content goes here */}
+        <h1>Welcome to Black Cat AI</h1>
+      </Container>
+      <Footer />
     </div>
   );
 }
